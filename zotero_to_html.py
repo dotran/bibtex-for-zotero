30bb153b103a47dbd3ada12d68f5b4b1cdc2513c
Do Tran <trantdo@gmail.com>
2017-08-14 02:35:39 +0200

    Redesign completely

--- zotero_to_html.py
+++ zotero_to_html.py
@@ -1,19 +1,57 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+"""
+Export the whole Zotero library (or a collection) to BibTeX using Better
+BibTeX and use the BibTeX2HTML tool to make nicely formatted HTML files with
+with abstracts, links, etc.
+
+NOTE #1:
+I decided to use the Zotero localhost utility along with Better BibTeX, as
+it's time consuming to pull items from the Zotero Web API (either with HTTP
+request or with the help of the "pyzotero" library), and it's also tough
+(though possible) to read from the local "zotero.sqlite"
+
+[see, http://www.texmacs.org/tmweb/miguel/task-zotero.en.html
+      https://github.com/smathot/qnotero/blob/master/libzotero/libzotero.py]
+
+NOTE #2:
+This is the UPDATED VERSION which splits the large bib database (exceeding
+the Hash size of 35000) into smaller junks.
+
+UPDATE:
+To sort the bib wrt "Date Added", we must have access to this field, which
+isn't available by default in the Better BibTeX translator for Zotero. Thus
+we need to add this field to the output by using an API of Better BibTeX
+which allows accessing the reference just before it's written out and
+cached. This can be achieved by adding the a JavaScript snippet to the
+Postscript box in the Advanced tab of the Better BibTeX preferences:
+
+if (Translator.BetterBibTeX) {
+    if (this.item.dateAdded) {
+        this.add({ name: 'dateadded', value: this.item.dateAdded });
+    }
+}
+
+We can use this method to add any Zotero field to the Better BibTeX .bib output.
+Ref: https://github.com/retorquere/zotero-better-bibtex/wiki/Scripting
+"""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import os
-import shutil
+import sys
 import time
 import bibutils
+
+from .zotero_to_bib import parse_args, zotero_to_bib
 
 
 EXCLUDED_FIELDS = (
     'note',
-    # 'isbn',
+    'isbn',
     # 'abstract',
     # 'month',
     # 'file',
@@ -29,74 +67,28 @@ EXCLUDED_FIELDS = (
     'pmcid',
     'dateadded')
 
-outfile  = "./newtest/testlib.bib"
+
+def bib_to_html(bib, output_file):
+    bib = sorted(bib,  # sort the database wrt 'year' for the HTML outputs
+                 key=lambda k: k['data']['year'] if 'year' in k['data'].keys() else 0,
+                 reverse=True)
+    
+    bibutils.format_output(bib,
+                           excluded_fields=EXCLUDED_FIELDS,
+                           keep_both_doi_url=True)
+    
+    bibutils.bib_to_html(bib,
+                         path=os.path.dirname(output_file),
+                         filename=os.path.basename(output_file))
 
 
-def zotero_to_html():
-    """
-    Export the whole Zotero library (or a collection) to BibTeX using Better BibTeX
-    and use the BibTeX2HTML tool to make nicely formatted HTML files with
-    with abstracts, links, etc.
-    
-    NOTE: This is the UPDATED VERSION which splits the large bib database
-    (exceeding the Hash size of 35000) into smaller junks.
-    
-    UPDATE: To sort the .bib wrt "Date Added", we must have access to this field,
-    which is not available by default in the Zotero Better BibTeX translator.
-    Thus we need to add this field to the output by using an API of Better BibTeX
-    which allows accessing the reference just before it's written out and cached.
-    This can be achieved by adding the a JavaScript snippet to the Postscript box
-    in the Advanced tab of the Better BibTeX preferences:
-    
-    if (Translator.BetterBibTeX) {
-        if (this.item.dateAdded) {
-            this.add({ name: 'dateadded', value: this.item.dateAdded });
-        }
-    }
-    
-    We can use this method to add any Zotero field to the Better BibTeX .bib output.
-    Ref: https://github.com/retorquere/zotero-better-bibtex/wiki/Scripting
-    """
-    
-    URL = "http://localhost:23119/better-bibtex/library?library.bibtex"
-    # URL = "http://localhost:23119/better-bibtex/collection?/0/QBN8FDDA.bibtex"  # for only one collection
-    # URL = "http://localhost:23119/better-bibtex/collection?/0/--GPU.bibtex"
-    
-    URL = "http://localhost:23119/better-bibtex/collection?/0/VWFR4ZR3.bibtex"
-    
-    
-    # Step 1: Export Zotero library/collection to a .bib file
-    new_bib = bibutils.read_zotero_localhost_bib(URL)
-    bibutils.fix_and_split(new_bib, omit_indecent_citekey=True)
-    new_bib = sorted(new_bib, key=lambda k: k['data']['dateadded'], reverse=False)  # sort by "Date Added"
-    bibutils.format_output(new_bib, excluded_fields=EXCLUDED_FIELDS, keep_both_doi_url=True)
-    
-    
-    # Sort the database wrt 'year' for the HTML outputs
-    new_bib_sorted = sorted(new_bib,
-                            key=lambda k: k['data']['year'] if 'year' in k['data'].keys() else 0,
-                            reverse=True)
-    
-    
-    bibutils.bib_to_html(bib=new_bib_sorted,
-                         path=os.path.dirname(outfile),
-                         filename=os.path.basename(outfile))
-    
-    
-    # Step 5: Format the generated .bib file to remove the fields file, abstract, etc.
-    # format_bib.main(infile=outfile, outfile=outfile)  # this is obsolete
-    bibutils.format_output(new_bib, excluded_fields=EXCLUDED_FIELDS + ('abstract', 'month', 'file'))
-    bibutils.write_bib_file(new_bib, 'outfile.bib')
-    
-    # Step 6 (additional): Check for any duplicate citation keys
-    bibutils.check_duplicate_citekeys(new_bib)
-
-
-def main():
-    zotero_to_html()
+def main(params):
+    url, output_html = parse_args(params, ext='.html')
+    new_bib = zotero_to_bib(url, output_html.replace('.html', '.bib'))
+    bib_to_html(new_bib, output_html)
 
 
 if __name__ == '__main__':
     startTime = time.time()
-    main()
+    main(sys.argv[1:])
     print('Processing done. It took: %1.2f' % (time.time() - startTime), 'seconds.')
