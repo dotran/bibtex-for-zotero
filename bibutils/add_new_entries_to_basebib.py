8c6445d17f5365fa72a6ba15ab43e9f906ce476f
Do Tran <trantdo@gmail.com>
2017-08-14 10:12:53 +0200

    Minor change to bibutils/add_new_entries_to_basebib.py

--- bibutils/add_new_entries_to_basebib.py
+++ bibutils/add_new_entries_to_basebib.py
@@ -6,17 +6,17 @@ from __future__ import division
 from __future__ import print_function
 
 
-def add_new_entries_to_basebib(basebib, newbib):
-    all_basebib_citekeys = [item['id'] for item in basebib]
+def add_new_entries_to_basebib(base_bib, new_bib):
+    basebib_citekeys = [item['id'] for item in base_bib]
     new_entries = []
     
-    for entry in newbib:
-        if entry['id'] not in all_basebib_citekeys:
+    for entry in new_bib:
+        if entry['id'] not in basebib_citekeys:
             new_entries.append(entry)
     
-    basebib += new_entries
+    base_bib += new_entries
     
     if new_entries:
-        print("%d new entries found and added to the basebib." % len(new_entries))
+        print("%d new entries found and added to the base bib." % len(new_entries))
     else:
-        print("No new entries found.")
+        print("No new entries found. Nothing to add to the base bib.")
