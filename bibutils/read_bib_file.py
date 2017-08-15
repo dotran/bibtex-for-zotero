3fde86ac0970a8c7e937681a2548365fd803c438
Do Tran <trantdo@gmail.com>
2017-08-15 05:18:33 +0200

    Minor: improve readability

--- bibutils/read_bib_file.py
+++ bibutils/read_bib_file.py
@@ -23,6 +23,7 @@ def read_bib_file(bibfile, omit_indecent_citekey=False, verbose=True):
     """
     if not os.path.isfile(bibfile):
         raise Exception("File to read not found:\n\t\%s" % bibfile)
+    
     with open(bibfile, 'rb') as f:
         buf = f.read().split('\n')
     buf = [line[:-1] if line.endswith('\r') else line for line in buf]  # remove '\r' in Windows files
